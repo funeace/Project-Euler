44a971ab23ecde2f2b4dd595d4f1eca85f006340
Kengo <funeace@gmail.com>
2020-05-25 11:46:16 +0900

    [Add]Problem027

--- Problem027.rb
+++ Problem027.rb
@@ -1,21 +1,199 @@
-# 1から初めて右方向に進み時計回りに数字を増やしていき, 5×5の螺旋が以下のように生成される:
-# 21  22  23  24  25
-# 20  7 8 9 10
-# 19  6 1 2 11
-# 18  5 4 3 12
-# 17  16  15  14  13
-# 両対角線上の数字の合計は101であることが確かめられる.
-# 1001×1001の螺旋を同じ方法で生成したとき, 対角線上の数字の和はいくつか?
+# オイラーは以下の二次式を考案している:
+# n2 + n + 41.
+# この式は, n を0から39までの連続する整数としたときに40個の素数を生成する.
+# しかし, n = 40 のとき 402 + 40 + 41 = 40(40 + 1) + 41 となり41で割り切れる.
+# また, n = 41 のときは 412 + 41 + 41 であり明らかに41で割り切れる.
+# 計算機を用いて, 二次式 n2 - 79n + 1601 という式が発見できた.
+# これは n = 0 から 79 の連続する整数で80個の素数を生成する. 係数の積は, -79 × 1601 で -126479である.
+# さて, |a| < 1000, |b| ≤ 1000 として以下の二次式を考える (ここで |a| は絶対値): 例えば |11| = 11 |-4| = 4である.
+# n2 + an + b
+# n = 0 から始めて連続する整数で素数を生成したときに最長の長さとなる上の二次式の, 係数 a, b の積を答えよ.
+# 理解が足りない
 
-# 対角線の値は中央の1,2ずつ増加するものが4つ、6つずつ増加するものが4つという規則がある
-  ans = 1
-  plus = 1
+# オイラーの法則
+def oiler(a,b,n)
+  n ** 2 + a * n + b
+end
 
-  (1..500).each do |i|
-    (0..3).each do |j|
-      plus += i * 2
-      ans += plus
-    end
+# 素数判定
+def prime?(n)
+  return true if n == 2 
+ 
+  limit = Math.sqrt(n).ceil
+  3.step(limit, 2) do |i|
+    return false if n % i == 0
   end
 
-p ans
+  return true
+end
+
+# ユークリッドの互除法
+def euclidean(a, b)
+  if a > b && b != 0
+    euclidean(a%b, b)
+  elsif a < b && a != 0
+    euclidean(a, b%a)
+  else
+    return (a > b ? a : b)
+  end
+end
+
+# 連続する素数の判定して、素数の数を数える
+def check_prime(a,b,ans)
+  max_n = ans[0]
+  answer = ans[1]
+
+  1.step do |n|
+    f = oiler(a,b,n)
+    break if f < 2
+    
+    unless prime?(f)
+      if max_n < n
+        puts "n = " + n.to_s + ", a = " + a.to_s + ", b = " + b.to_s
+        max_n = n
+        answer = a*b
+      end
+      break
+    end
+  end
+  
+  return [max_n, answer]
+end
+
+# 探索開始
+ans = [0, 0]
+
+# n = 0を考えるとbは素数
+# b = 2, n = 1のときfunc = a + 3
+# これが素数になりうるのはaが偶数のときのみ
+# a = even, b = 2
+2.step(1000, 2) do |a|
+  ans = check_prime(a, 2, ans)
+  ans = check_prime(-a, 2, ans)
+
+end
+
+
+# bを求める
+1.step(1000, 2) do |a|
+  ans = check_prime(a, 2, ans)
+  ans = check_prime(-a, 2, ans)
+
+  
+  3.step(1000, 2) do |b|
+    next if euclidean(a, b) != 1
+    next unless prime?(b)
+
+    # a:+, b:+
+    ans = check_prime(a, b, ans)
+    ans = check_prime(-a, b, ans)
+  end
+end
+
+puts ans
+
+
+
+
+# さんこうにしt答え
+# coding: utf-8
+# Problem 27
+
+# 関数の値を返す
+# def func(a,b,n)
+#   a = a.to_i
+#   b = b.to_i
+#   n = n.to_i
+#   return n*n + a*n + b
+# end
+
+# # 引数が素数かどうか調べる
+# def check_if_prime(n)
+#   return true if n == 2 
+ 
+#   limit = Math.sqrt(n).ceil
+#   3.step(limit, 2) do |i|
+#     return false if n % i == 0
+#   end
+
+#   return true
+# end
+
+# # ユークリッドの互除法で最大公約数を求める
+# def euclidean(a, b)
+#   if a > b && b != 0
+#     euclidean(a%b, b)
+#   elsif a < b && a != 0
+#     euclidean(a, b%a)
+#   else
+#     return (a > b ? a : b)
+#   end
+# end
+
+# # funcをn=0から順に呼び出して連続していくつ素数になるか調べる
+# def prime_chain(a, b, ans)
+#   max_n = ans[0]
+#   answer = ans[1]
+
+#   1.step do |n|
+#     f = func(a,b,n)
+#     break if f < 2
+    
+#     if !check_if_prime(f)
+#       if max_n < n
+#         puts "n = " + n.to_s + ", a = " + a.to_s + ", b = " + b.to_s
+#         max_n = n
+#         answer = a*b
+#       end
+#       break
+#     end
+#   end
+  
+#   return [max_n, answer]
+# end
+
+# # 探索開始
+# ans = [0, 0]
+
+# # n = 0を考えるとbは素数
+
+# # b = 2, n = 1のときfunc = a + 3
+# # これが素数になりうるのはaが偶数のときのみ
+# # a = even, b = 2
+# 2.step(1000, 2) do |a|
+#   # a:+, b:+
+#   ans = prime_chain(a, 2, ans)
+
+#   # a:-, b:+
+#   ans = prime_chain(-a, 2, ans)
+# end
+
+# # b>2ならばbは奇数
+# # n = 1のときfunc = a+b+1,b+1は偶数なのでaは奇数
+# # a = odd, b = prime(>2)
+# 1.step(1000, 2) do |a|
+
+#   # b = 2
+#   # a:+, b:+
+#   ans = prime_chain(a, 2, ans)
+
+#   # a:-, b:+
+#   ans = prime_chain(-a, 2, ans)
+
+  
+#   3.step(1000, 2) do |b|
+#     next if euclidean(a, b) != 1
+#     next if !check_if_prime(b)
+
+#     # a:+, b:+
+#     ans = prime_chain(a, b, ans)
+
+#     # a:-, b:+
+#     ans = prime_chain(-a, b, ans)
+
+#     # b<0のときfunc(n=0)が負なので調べなくて良い
+    
+#   end
+# end
+
+# puts ans.to_s
