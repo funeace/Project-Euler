8425587e4f5b4e822d1cdecf63fe51563338cc3a
Kengo <funeace@gmail.com>
2020-06-02 21:45:58 +0900

    [Add]Problem033

--- Problem033.rb
+++ Problem033.rb
@@ -15,8 +15,6 @@
 # denominator 分母
 # molecure 分子
 
-# 回答あわない
-
 def elimination?(molecule, denominator)
   # 自明な例はfalse
   m_div,m_mod = molecule.divmod(10)
@@ -28,12 +26,62 @@ end
 
 answer = []
 
-(10..99).each do |x|
-  (10..99).each do |y|
+(10..98).each do |x|
+  (x + 1..99).each do |y|
+    p x/y
     if elimination?(x,y)
-      answer.push(x/y)
+      answer.push(x.div(y))
     end
   end
 end
 
+# p answer
 p answer.sum
+
+# 他の人の回答
+
+# ユークリッドの互除法で最大公約数を求める
+def euclidean(a, b)
+  if a > b && b != 0
+    euclidean(a%b, b)
+  elsif a < b && a != 0
+    euclidean(a, b%a)
+  else
+    return (a > b ? a : b)
+  end
+end
+
+answer_num = 1
+answer_den = 1
+10.step(99) do |a|
+  (a+1).step(99) do |b|
+    next if a%10 == 0 && b%10 == 0
+
+    flg = false
+    common = ''
+    a.to_s.each_char do |c|
+      if b.to_s.include?(c)
+        common = c
+        flg = true
+      end
+    end
+    next if !flg
+
+    max_div = euclidean(a, b)
+    next if max_div == 1
+
+    removed_a = a.to_s.delete(common).to_i
+    removed_b = b.to_s.delete(common).to_i
+    max_div_removed = euclidean(removed_a, removed_b)
+    next if removed_b == 0
+
+    if a/max_div == removed_a/max_div_removed && b/max_div == removed_b/max_div_removed
+      puts "#{a} / #{b},  #{a/max_div} / #{b/max_div}"
+      answer_num = answer_num * a
+      answer_den = answer_den * b
+    end
+    
+  end
+end
+
+puts (answer_den/euclidean(answer_num, answer_den)).to_s
